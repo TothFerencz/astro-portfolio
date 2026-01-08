a53c64db360fcb0152995b3a93bfa7104c251260
tothferencz <ferencztothdev@gmail.com>
2026-01-08 21:09:15 +0200

    fix: contact, vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  output: 'static',
+  output: 'server',
+  adapter: vercel(),
   vite: {
     plugins: [tailwindcss()],
   },
