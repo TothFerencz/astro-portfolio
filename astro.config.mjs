a0339de069e4db4d38f6b05895d232514872110e
tothferencz <ferencztothdev@gmail.com>
2026-01-08 21:50:01 +0200

    add: sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/static';
+import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  output: 'server',
+  site: 'tothferencz.com',
+  output: 'static',
   adapter: vercel(),
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
